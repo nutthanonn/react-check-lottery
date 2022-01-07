012ba3416faf36adcde8dbf90b11a48327b13ffb
nutthanon <nutthanon.tho@gmail.com>
2022-01-07 19:52:54 +0700

    [+] api

--- src/api/dataPrize.ts
+++ src/api/dataPrize.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
+import { prizeProps } from "../interfaces/lotteryPrize";
 
 export async function DataPrize(id: string) {
   const res = await axios.get(`https://lotto.api.rayriffy.com/lotto/${id}`);
-  return res.data.response;
+
+  const data = await res.data.response;
+
+  const prizeNumberShow: prizeProps[] = [
+    data.prizes[0],
+    ...res.data.response.runningNumbers,
+  ];
+
+  return [data, prizeNumberShow];
 }
